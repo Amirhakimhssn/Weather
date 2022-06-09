7ccff740dbdf7cb6379810eb77cac9c02bfd5c50
Amirhakimhssn <107099195+Amirhakimhssn@users.noreply.github.com>
2022-06-09 13:33:45 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -28,9 +28,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+rain = pd.read_csv('https://raw.githubusercontent.com/Amirhakimhssn/Weather/main/seattle-weather(nodate).csv')
+X = rain.drop['weather']
+Y = rain['weather']
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -39,10 +39,10 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write(rain.weather_names)
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
+st.write(rain.weather_names[prediction])
 #st.write(prediction)
 
 st.subheader('Prediction Probability')
