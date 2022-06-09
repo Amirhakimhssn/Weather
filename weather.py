41de04d1d1cfeffdd678eea4256c46c2966eebf3
Amirhakimhssn <107099195+Amirhakimhssn@users.noreply.github.com>
2022-06-09 13:29:14 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,22 +4,22 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""    
-# Simple Iris Flower Prediction App
+# Weather Predictor App
 
-This app predicts the **Iris flower** type!
+This app predicts the **Weather** using a set of parameters
 """)
 
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
+    precipitation = st.sidebar.slider('Precipitation', 0.0, 55.9, 0.0)
+    temp_max = st.sidebar.slider('Max Temperature', -1.6, 35.6, 30)
+    temp_min = st.sidebar.slider('Min Temperature', -7.1, 18.3, 11.1)
+    wind = st.sidebar.slider('Wind', 0.4, 9.5, 6)
+    data = {'precipitation': precipitation,
+            'temp_max': temp_max,
+            'temp_min': temp_min,
+            'wind': wind}
     features = pd.DataFrame(data, index=[0])
     return features
 
