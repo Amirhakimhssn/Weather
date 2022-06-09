2c411a589ec650037d761cae2894d06fea6d9de0
Amirhakimhssn <107099195+Amirhakimhssn@users.noreply.github.com>
2022-06-09 16:04:19 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -38,11 +38,11 @@ clf.fit(X, Y)
 prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
-st.subheader('Class labels and their corresponding index number')
+st.subheader('Weather labels and their corresponding index number')
 st.write(Y.unique())
 
 st.subheader('Prediction')
 st.write(prediction)
 
 st.subheader('Prediction Probability')
-st.write(['drizzle', 'rain', 'sun', 'snow', 'fog'],prediction_proba)
+st.write(prediction_proba)
